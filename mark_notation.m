1120b7d47fa4144d32843d925fa4ada672c7e860
Boyi Lee <boylee1111@gmail.com>
2015-03-22 14:31:35 +0800

    add "mark notation" snippet

---
+++ mark_notation.m
@@ -0,0 +1,10 @@
+// Mark Notation
+// Notation of "pragma mark" with separator
+//
+// IDECodeSnippetCompletionPrefix: mark
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 0CF74657-CDD8-4ECC-BA22-E6DF0965FBBC
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 0
+#pragma mark - <#section#>
