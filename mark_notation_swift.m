45a877c085ecd6dd379d2ef3f23d3db8de728373
Boyi Lee <boylee1111@gmail.com>
2015-04-12 18:06:39 +0800

    Imported user code snippets

---
+++ mark_notation_swift.m
@@ -0,0 +1,10 @@
+// Mark Notation Swift
+// Notation of "// MARK:" without separator with Swift format
+//
+// IDECodeSnippetCompletionPrefix: mark
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 2038C915-E90A-4186-95BE-59A410135114
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Swift
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+// MARK: <#section#>
