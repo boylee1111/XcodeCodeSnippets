4dcf808ebe4d88f91d877087cfd79182e320f1b4
Boyi Lee <boylee1111@gmail.com>
2015-04-12 18:11:50 +0800

    Imported user code snippets

---
+++ objective-c_mark_notation.m
@@ -0,0 +1,9 @@
+// Objective-C Mark Notation
+// Notation of "pragma mark" without separator with Objective-C format
+//
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: AEA76EFD-410F-4A93-A106-5D9C653719B2
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#pragma mark <#section#>
